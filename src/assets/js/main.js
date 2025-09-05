92cdc062ad6bb8615b2557f8d1db9aaefaa7f021
anitaparmar26 <easeanita@gmail.com>
2025-09-05 13:10:14 +0530

    added filter and updated theme

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,40 @@
-$(document).ready(function() {
-    "user strict";
-    if ($('[data-toggle="tooltip"]').length) {
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-    }
-});
+// $(document).ready(function() {
+//     "user strict";
+//     if ($('[data-toggle="tooltip"]').length) {
+//         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+//         var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+//             return new bootstrap.Tooltip(tooltipTriggerEl)
+//         })
+//     }
+// });
+
+// Offcanvas
+
+var navbar = document.querySelector('.navbar');
+
+const navOffCanvasBtn = document.querySelectorAll('.offcanvas-nav-btn');
+const navOffCanvas = document.querySelector('.navbar:not(.navbar-clone) .offcanvas-nav');
+
+// Declare bsOffCanvas outside the if block
+let bsOffCanvas;
+
+// Check if the navOffCanvas element exists before creating the Offcanvas instance
+if (navOffCanvas) {
+  bsOffCanvas = new bootstrap.Offcanvas(navOffCanvas, { scroll: true });
+
+  // Event listeners
+  navOffCanvasBtn.forEach((e) => {
+    e.addEventListener('click', (event) => {
+      toggleOffCanvas();
+    });
+  });
+}
+
+// Function to toggle the Offcanvas
+function toggleOffCanvas() {
+  if (bsOffCanvas && bsOffCanvas._isShown) {
+    bsOffCanvas.hide();
+  } else if (bsOffCanvas) {
+    bsOffCanvas.show();
+  }
+}
